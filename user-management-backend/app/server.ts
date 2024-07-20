07b45901954435c48820772d93ca9acfa6336a8c
suitsgroup <suitsroleplay@gmail.com>
2024-07-20 13:48:24 -0300

    chore: estrutura inicial do backend

---
+++ user-management-backend/app/server.ts
@@ -0,0 +1,30 @@
+import fastify from "fastify";
+import postgres from "@fastify/postgres";
+
+import consola from "consola";
+import chalk from "chalk";
+
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = fastify();
+
+app.register(require("@fastify/cors"), { origin: true }); // Permite o acesso de qualquer origem.
+app.register(postgres, { connectionString: process.env.DB_URL }); // Conectando o servidor ao banco de dados.
+
+app.get("/", async (request, reply) => {
+  return { hello: "world" };
+});
+
+// Ligando o servidor a porta 3000, host 0.0.0.0.
+app.listen({ port: 3000, host: '0.0.0.0' }, (err, address) => {
+  if (err) {
+    consola.error(err);
+    process.exit(1);
+  }
+
+  consola.success(
+    chalk.green(`Servidor está online em: http://localhost:3000`)
+  );
+});
