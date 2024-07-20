e29b22f48fb0d088d0d50392831dd8bf4e4a0a51
suitsgroup <suitsroleplay@gmail.com>
2024-07-20 20:02:20 -0300

    fix: Corrigir o problema onde o tratamento de erros personalizados não estava retornando corretamente

--- user-management-backend/app/server.ts
+++ user-management-backend/app/server.ts
@@ -1,15 +1,27 @@
-import fastify from "fastify";
+import fastify, { FastifyError } from "fastify";
 
 import consola from "consola";
 import chalk from "chalk";
 
 import dotenv from "dotenv";
+import { InternalError } from "./errors/InternalError";
 
 const app = fastify(); // Cria uma instância do fastify.
 
 dotenv.config();
-app.register(require("@fastify/cors"), { origin: true }); // Permite o acesso de qualquer origem.
 
+app.setErrorHandler(function (error: FastifyError, _, reply) {
+  if (error instanceof InternalError) {
+    reply.status(error.statusCode).send({
+      statusCode: error.statusCode,
+      message: error.messages,
+    });
+  } else {
+    reply.send(error);
+  }
+});
+
+app.register(require("@fastify/cors"), { origin: true }); // Permite o acesso de qualquer origem.
 app.register(require("./routes/user.route"), { prefix: "/users" }); // Rotas de usuários.
 
 // Ligando o servidor a porta 3000, host 0.0.0.0.
